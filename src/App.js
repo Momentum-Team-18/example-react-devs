b5234940e25d2522ad5ad9f6b725a9f655379d5b
Amy Gori <amydgori@gmail.com>
2023-06-06 16:16:08 -0400

    A small application very similiar to freeshelf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css'
-import bookData from './book-data.json'
+// import bookData from './book-data.json'
+import Dev from './Dev'
 
 function App() {
+  const devs = ['Anna', 'Teddy', 'Meagan', 'Alex']
+
   return (
     <div>
-      <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
+      <h1>React Devs for Hire!</h1>
+      <ul>
+        {devs.map((dev) => {
+          return <Dev name={dev} />
+        })}
+      </ul>
     </div>
   )
 }
 
 export default App
+
+// important things in React:
+// JSX
+// components
+// props
+// state
